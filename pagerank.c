fc784a7fbfe9a9d0482de7a3adc7dd6669fd6c10
Zhuosheng Jiang <jemit.uriel@gmail.com>
2015-10-06 19:19:43 -0700

    aa

--- pagerank.c
+++ pagerank.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <mpi.h>
+
 int main(int argc, char *argv[]) {
   int numprocs, rank, namelen;
   char processor_name[MPI_MAX_PROCESSOR_NAME];
@@ -7,12 +9,17 @@ int main(int argc, char *argv[]) {
   char * line = NULL;
   size_t len = 0;
   ssize_t read;
-  char * pch;
+
+  char* token;
+  char* string;
+  char* tofree;
+
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Get_processor_name(processor_name, &namelen);
   printf("Process %d on %s out of %d\n", rank, processor_name, numprocs);
+
   if(rank == 0){
 
     fp = fopen("./graphs/200K-graph.txt", "r");
@@ -21,19 +28,24 @@ int main(int argc, char *argv[]) {
     }
 
     while ((read = getline(&line, &len, fp)) != -1) {
-      printf("Retrieved line of length %zu :\n", read);
-      pch = strtok (line ," ");
-      while (pch != NULL)
-      {
-        // printf ("%s\n",pch);
-        pch = strtok (NULL, " ");
-      }
-      printf('\n');
-    }
+      string = line;
 
+      if (string != NULL) {
+
+        tofree = string;
+
+        while ((token = strsep(&string, ",")) != NULL)
+        {
+          printf("%s\n", token);
+        }
+
+        free(tofree);
+      }
+    }
     fclose(fp);
-    if (line)
-    free(line);
+    if (line){
+      free(line);
+    }
   }
 
   MPI_Finalize();
