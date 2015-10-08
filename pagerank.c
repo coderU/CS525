2d55f3a9b5cd2e301bb7e3b61f81190bd57a23b0
Zhuosheng Jiang <jemit.uriel@gmail.com>
2015-10-08 00:07:09 -0700

    aa

--- pagerank.c
+++ pagerank.c
@@ -196,7 +196,7 @@ int main(int argc, char *argv[]) {
     // }
   }
   MPI_Bcast(&l_val_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(&val, l_val_size, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&val, l_val_size, MPI_FLOAT, 0, MPI_COMM_WORLD);
   MPI_Barrier(MPI_COMM_WORLD);
   printf("%d: %d\n",rank, l_val_size -1);
 
