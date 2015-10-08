ed628dcfa9b132cb9e2dfe562f4cda82db89850c
Zhuosheng Jiang <jemit.uriel@gmail.com>
2015-10-08 10:13:07 -0700

    aa

--- pagerank.c
+++ pagerank.c
@@ -175,10 +175,10 @@ int main(int argc, char *argv[]) {
       *(subgraph_index+i)=0;
     }
 
-    // for(i = 0 ; i < part_count ; i++){
-    //   *(subgraph_count+*(part+i)) = *(subgraph_count+*(part+i))+1;
-    // }
-    //
+    for(i = 0 ; i < part_count ; i++){
+      *(subgraph_count+*(part+i)) = *(subgraph_count+*(part+i))+1;
+    }
+
     // for(i = 0 ; i < (max+1) ; i++){
     //   *(subgraph+i)=(int*)malloc(*(subgraph_count+i)*sizeof(int));
     // }
