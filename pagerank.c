bfa3fb76bdd7aa18276891e8f4081bd83c81d83b
Zhuosheng Jiang <jemit.uriel@gmail.com>
2015-10-08 00:51:20 -0700

    aa

--- pagerank.c
+++ pagerank.c
@@ -226,7 +226,7 @@ int main(int argc, char *argv[]) {
 
   for(i = 0 ; i < size - 1 ; i++){
     for(j = 0 ; j < size - 1 ; j++){
-      printf("%d %d %f\n", i, j *(*(matrix+i)+j));
+      printf("%d %d %f\n", i, j ,*(*(matrix+i)+j));
       // *(*(matrix+i)+j) = 0;
     }
     printf("\n");
