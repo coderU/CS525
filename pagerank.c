64b5474bce3fba088f7475c22e560bf48ba7a687
Zhuosheng Jiang <jemit.uriel@gmail.com>
2015-10-08 17:45:44 -0700

    aa

--- pagerank.c
+++ pagerank.c
@@ -380,7 +380,7 @@ int main(int argc, char *argv[]) {
         combine_vector(l_vector, t_vector, size-1);
       }
 
-      // printf("At iteration %d, we have original:%f, current:%f\n",iteration, *(vector+196498),*(l_vector+196498) );
+      printf("At iteration %d, we have original:%f, current:%f\n",iteration, *(vector+196498),*(l_vector+196498) );
       ok = calculate_diff(vector,l_vector, size-1);
       memcpy(vector,l_vector,size-1);
       // printf("At iteration %d, we have original:%f, current:%f\n", iteration, *(vector+196498), *(l_vector+196498) );
