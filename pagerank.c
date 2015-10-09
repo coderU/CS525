b52055eefcb0b8c3d2c5017b4d756d14e812cf7a
Zhuosheng Jiang <jemit.uriel@gmail.com>
2015-10-09 10:29:24 -0700

    aa

--- pagerank.c
+++ pagerank.c
@@ -510,7 +510,7 @@ int main(int argc, char *argv[]) {
 
   int iteration = 1;
   int ok = 0;
-  while(iteration < 3){
+  while(1 < 3){
     if(rank == 0){
       //DISTRIBUTE ALL NECCESSERY VECTOR ELEMENTS
       for(i = 1 ; i < (max+1) ; i++){
