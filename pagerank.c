bef926bd3538e823c67982ce6ed8c5a1117ca773
Zhuosheng Jiang <jemit.uriel@gmail.com>
2015-10-08 19:14:07 -0700

    aa

--- pagerank.c
+++ pagerank.c
@@ -245,7 +245,7 @@ int main(int argc, char *argv[]) {
     int temp[size-1];
     memset(temp, 0, size-1);
     for(i = 0 ; i < l_val_size - 1 ; i++){
-      temp[*(col+i)]++;
+      temp[*(col+i)] = temp[*(col+i)]+1;
     }
     for(i = 0 ; i < l_val_size - 1 ; i++){
       *(val+i) = *(val+i)/temp[*(col+i)];
