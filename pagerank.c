e80497af19646711bd6c6118beb82fd503c75db0
Zhuosheng Jiang <jemit.uriel@gmail.com>
2015-10-09 18:30:54 -0700

    aaa

--- pagerank.c
+++ pagerank.c
@@ -267,6 +267,7 @@ int main(int argc, char *argv[]) {
     for(i = 0 ; i < (max+1) ; i++){
       printf("Node %d has: %d elements\n",i, *(subgraph_count+i) );
     }
+    printf("aaaaaaaaaaaaaaa\n" );
     for(i = 0 ; i < (max+1) ; i++){
       *(subgraph+i)=(int*)malloc(*(subgraph_count+i)*sizeof(int));
       for(j = 0 ; j < *(subgraph_count+i) ; j++){
