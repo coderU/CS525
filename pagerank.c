9d5ee43c66d7f9906b115fcfa872a52bc7495b2e
Zhuosheng Jiang <jemit.uriel@gmail.com>
2015-10-13 22:22:17 -0700

    aa

--- pagerank.c
+++ pagerank.c
@@ -472,6 +472,17 @@ int main(int argc, char *argv[]) {
 
     for(i = 1 ; i < (max+1) ; i++){
       MPI_Recv(t_vector, (size-1), MPI_FLOAT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      float* origin = (float*)malloc(*(subgraph_count+i)*sizeof(float));
+      MPI_Recv(origin, *(subgraph_count+i), MPI_FLOAT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+      float* temp_vector = (float*)malloc((size-1)*sizeof(float));
+      for( j = 0 ; j < (size-1) ; j++){
+        *(temp_vector+j) = 0;
+      }
+      for( j = 0 ; j < *(subgraph_count+i) ; j++){
+        *(temp_vector+*(*(subgraph+i)+j)) = *(origin+j);
+      }
+      compare_vector(temp_vector, t_vector,*(subgraph_count+i),i);
       combine_vector(l_vector, t_vector, size-1);
     }
     my_memcpy(vector,l_vector,size-1);
@@ -532,7 +543,7 @@ int main(int argc, char *argv[]) {
       fprintf(stderr, "b: %d/%d %d\n", i,(local_subgraph_count), *(local_subgraph+i));
       *(local_subgraph_vector+i) = *(l_vector+*(local_subgraph+i));
     }
-
+    MPI_Send(local_subgraph_vector, (local_subgraph_count), MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
   }
   MPI_Barrier(MPI_COMM_WORLD);
   printf("------------------------Start Computing!----------------------------\n" );
